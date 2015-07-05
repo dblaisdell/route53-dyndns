ee07ae64c561b4e74d6e45dff13631babfb91b8f
lunkdjedi <lunk.djedi@gmail.com>
2015-07-05 19:43:38 -0400

    Initial Project Structure

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject route53-dyndns "0.1.0-SNAPSHOT"
+  :description "Amazon Route53 Dynamic DNS"
+  :url "https://github.com/dblaisdell/route53-dyndns"
+  :license {:name "GPL v2"
+            :url "https://www.gnu.org/licenses/gpl-2.0.txt"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [amazonica "0.3.28"]])
---
+++ test/route53_dyndns/core_test.clj
@@ -0,0 +1,7 @@
+(ns route53-dyndns.core-test
+  (:require [clojure.test :refer :all]
+            [route53-dyndns.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

891900e5c497391195b98e19b210f2ad27ef0cb9
lunkdjedi <lunk.djedi@gmail.com>
2015-07-05 21:07:22 -0400

    Added comments for updating dns function

--- src/route53_dyndns/core.clj
+++ src/route53_dyndns/core.clj
@@ -6,7 +6,6 @@
   (let [zones (list-hosted-zones)]
     (map :name (:hosted-zones zones))))
 
-
 (defn create-change [hostname ip]
   (let [rr (list (ResourceRecord. ip))
         rrs (ResourceRecordSet. hostname "A")
@@ -16,8 +15,15 @@
     (.setTTL rrs 300)
     cb))
 
-
 (defn external-ip []
   (clojure.string/trim (slurp "http://icanhazip.com/")))
 
+
+;(change-resource-record-sets 
+;  :hosted-zone-id "Z1SFTI1M7NYHOB" 
+;  :change-batch (create-change "test.digitaljedi.com" (external-ip)))
+
+;To make the above work
+;1) Convert hostname to domain
+;2) Resolve domain zoneid
 
